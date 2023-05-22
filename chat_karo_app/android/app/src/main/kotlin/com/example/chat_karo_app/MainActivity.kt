e351ebbdf699a9a90c77637ac9e3be820dd9784c
Kishan <sharmilakishan.2003@gmail.com>
2023-05-22 10:37:56 +0530

    Cash-karo-app

---
+++ chat_karo_app/android/app/src/main/kotlin/com/example/chat_karo_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chat_karo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- q_w/android/app/src/main/kotlin/com/example/q_w/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.q_w
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
