2fe2bccb480fcbc1db346823f1d36594e0549707
T.S. Hayagriva Kishan <sharmilakishan.2003@gmail.com>
2023-07-25 07:47:11 +0530

    working on button to render the UI to Switchscreen

---
+++ adv_basics/lib/quiz.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:adv_basics/questions_screen.dart';
+import 'package:adv_basics/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchscreen);
+    super.initState();
+  }
+
+  void switchscreen() {
+    setState(() {
+      activeScreen = const QuestionScreen();
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 132, 80, 221),
+                Color.fromARGB(255, 85, 42, 161),
+              ],
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
